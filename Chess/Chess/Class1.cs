3c783ad5a472dc0241aa31f00552a9cb4723ffbe
ShirVyach <64410243+ShirVyach@users.noreply.github.com>
2020-12-15 16:04:47 +0500

    Add files via upload

---
+++ Chess/Chess/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Horse : IFigure
+    {        
+        readonly List<Char> letters = new List<Char> {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
+        readonly List<Char> numbers = new List<Char> { '1', '2', '3', '4', '5', '6', '7', '8' };
+        public string Pos { get; set; }
+        public string Name { get; set; }
+
+        public bool CheckPos(string Pos1)
+        {
+            int x = letters.FindIndex(i => i == Pos[0]);  // буква 
+            int y = numbers.FindIndex(i => i == Pos[1]); // цифра 
+            if ((Math.Abs(letters.FindIndex(i => i == Pos1[0]) - x) == 2 & Math.Abs(numbers.FindIndex(i => i == Pos1[1]) - y) == 1) ||
+                (Math.Abs(letters.FindIndex(i => i == Pos1[0]) - x) == 1 & Math.Abs(numbers.FindIndex(i => i == Pos1[1]) - y) == 2))
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+
+    public class Rook : IFigure
+    {
+        public string Pos { get; set; }
+        public string Name { get; set; }
+
+        public bool CheckPos(string Pos1)
+        {
+            char x = Pos[0];  // буква 
+            char y = Pos[1]; // цифра 
+            if (Pos1[0] != x & Pos1[1] != y)
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            };
+        }
+
+    }
+}
---
+++ Chess/Chess/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main()
+        {
+            string FigPos = Console.ReadLine();
+            Horse horse = new Horse();
+            Rook rook = new Rook();
+
+            string text = "";
+
+            using (StreamReader fs = new StreamReader(@"chess.txt"))
+            {
+                while (true)
+                {
+                    // Читаем строку из файла во временную переменную.
+                    string temp = fs.ReadLine();
+
+                    // Если достигнут конец файла, прерываем считывание.
+                    if (temp == null) break;
+
+                    // Пишем считанную строку в итоговую переменную.
+                    text += temp+" ";
+                }
+            }
+
+            List<IFigure> lst = new List<IFigure>() { };
+
+            string[] text2 = text.Split(' ');
+
+            for (int i = 0; i < text2.Length; i += 2)
+            {
+                if (text2[i] == "R")
+                {
+                    rook.Pos = text2[i + 1];
+                    rook.Name = text2[i];
+
+                    lst.Add(rook);
+                    if (!rook.CheckPos(FigPos))
+                    {
+                        Console.WriteLine(false);
+                        Console.ReadLine();
+                        return;  
+                    }
+                }
+
+                if (text2[i] == "H")
+                {
+
+                    horse.Pos = text2[i + 1];
+                    horse.Name = text2[i];
+
+                    lst.Add(horse);
+                    if(horse.CheckPos(FigPos) == false)
+                    {
+                        Console.WriteLine(false);
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine(true);
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
